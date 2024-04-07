a28fbde867c50dea49fcefc04e7ac1f1e7de5464
vbraychenko <rybachok@ukr.net>
2024-04-07 12:30:02 +0300

    Modify Main.java > add word "My"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello beautiful world!");
+        System.out.println("Hello My beautiful world!");
     }
 
 }
